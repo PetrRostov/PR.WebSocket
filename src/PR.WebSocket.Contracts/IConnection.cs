2b1f9d85aa26fe10bc8ed9570a7b2dfe18b61229
Petr Rostov <petr.rostov@gmail.com>
2013-03-20 17:06:56 +0400

    init

---
+++ src/PR.WebSocket.Contracts/IConnection.cs
@@ -0,0 +1,14 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+
+namespace PR.WebSocket.Contracts
+{
+    public interface IConnection
+    {
+        IPEndPoint IP { get; }
+
+        Task SendAsync(string msg);
+
+        void Send(string msg);
+    }
+}
